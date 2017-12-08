b83f01869f6c72fae59bcfde817eb232ab014b12
Onur Eker <yonureker@gmail.com>
2017-12-08 23:31:10 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  counter = 0
+  board.each do |i|
+    if |i| == "X" || |i| == "Y"
+      counter += 1
+    end
+  end
+end
+
+  
+  # counter at 0
+  # for element in board
+  #   if element == "X" or "Y"
+  #     add +1 to counter
+  #   else do nothing
+  # end
+end
+
+def current_player(board)
+end
